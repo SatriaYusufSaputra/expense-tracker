0026ed8d4276c468ba9ee022259933be33e47712
Satria Yusuf Saputra <satriasys2@gmail.com>
2026-06-22 08:06:42 +0700

    FIX BUG

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+
 import { connectDB } from "./db/connection.js";
 import expenseRoutes from "./routes/expenses.js";
 import authRoutes from "./routes/auth.js";
@@ -11,12 +12,14 @@ dotenv.config();
 
 const app = express(); // ✅ app harus didefinisikan DULU sebelum app.use()
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://expense-tracker-kohl-phi-25.vercel.app",
-  ]
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://expense-tracker-kohl-phi-25.vercel.app",
+    ],
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
