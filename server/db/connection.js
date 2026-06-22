7bcc57d168a8c377d48c6957fca292d900e1821d
Satria Yusuf Saputra <satriasys2@gmail.com>
2026-06-22 08:07:51 +0700

    fix bug

--- server/db/connection.js
+++ server/db/connection.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MANGO_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
